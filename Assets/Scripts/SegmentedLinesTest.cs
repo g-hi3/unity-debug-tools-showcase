540ac61a12a702c14ee0440c2dc1cda14b145562
g-hi3 <marc.forrer@bluewin.ch>
2025-08-07 18:00:57 +0200

    added simple example to show segmented line animations

---
+++ Assets/Scripts/SegmentedLinesTest.cs
@@ -0,0 +1,31 @@
+﻿using G_hi3.Debug;
+using UnityEngine;
+
+public sealed class SegmentedLinesTest : MonoBehaviour
+{
+    [SerializeField]
+    private Transform start;
+
+    [SerializeField]
+    private Transform end;
+
+    [SerializeField]
+    private Color segmentColor;
+
+    [SerializeField]
+    private float segmentLength;
+
+    [SerializeField]
+    private float segmentSpacing;
+
+    [SerializeField]
+    private float timeScale;
+
+    [SerializeField]
+    private bool depthTest;
+
+    private void Update()
+    {
+        DebugTools.DrawSegmentedLine(start.position, end.position, segmentLength, segmentSpacing, segmentColor, timeScale, depthTest);
+    }
+}
